7cad280cbdc40666db29b920adc40b5db2fe538d
kevin578 <kbriggs@2600-6c64-607f-f8a6-0c42-a10c-4f35-1bab.dhcp6.chtrptr.net>
2017-10-09 18:52:35 -0400

    button shows the equations

--- math game/ViewController.swift
+++ math game/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //declare variables
+    var number1 = 0
+    var number2 = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //when the button is pressed it sets the random numbers and picks an operator
+    @IBAction func checkAnswer(_ sender: Any) {
+        number1 = Int(arc4random_uniform(UInt32(100)))
+        number2 = Int(arc4random_uniform(UInt32(100)))
+        let op = Int(arc4random_uniform(UInt32(2)))
+        checkAnswer(op: op)
     }
-
+    
+    //this prints the equation and the correct answer
+    func checkAnswer(op: Int){
+        if (op == 0) {
+            correctAnswer =  number1 + number2
+            print("\(number1) + \(number2)")
+            print(correctAnswer)
+        }
+            
+        else if (op == 1) {
+            correctAnswer = number1 - number2
+            print("\(number1) - \(number2)")
+            print(correctAnswer)
+        }
+            
+        else if (op == 2) {
+            correctAnswer = number1 * number2
+            print("\(number1) * \(number2)")
+            print(correctAnswer)
+        }
+        
+    }
 
 }
 
